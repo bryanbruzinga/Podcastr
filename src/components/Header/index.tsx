c012f225caadabee22ebb928ac26d519b425f669
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-04-20 08:49:37 -0300

    header crcriado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+export function Header () {
+    return (
+        <header className={styles.headerContainer}>
+            <img src="/logo.svg" alt="Podcastr" />
+            <p>O melhor para voce ouvir, sempre</p>
+
+            <span>Qui, 8 Abril</span>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, {Html, Head, Main, NextScript} from 'next/document'
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+//This needs to be created like this in order to add aditional featurings like custom fonts
