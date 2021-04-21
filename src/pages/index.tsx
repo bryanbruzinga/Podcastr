9c475114c4c39efcbb0aba49dc232a48af3a3968
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-04-21 12:17:42 -0300

    página dos episódios finalizada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link'
 import Image from 'next/image'
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
@@ -10,7 +11,6 @@ type Episode = {
   id: string;
   title: string;
   thumbnail: string;
-  description: string;
   members: string;
   duration: number;
   durationAsString: string;
@@ -36,7 +36,9 @@ export default function Home({latestEpisodes, allEpisodes} : HomeProps) {
                 <Image width={192} height={192} src={episode.thumbnail} alt={episode.title} objectFit="cover"/>
                 {/* Image tag needs next.config.js to work, u need to add your image domains there*/}
                 <div className={styles.episodeDetails}>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
                   <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
@@ -54,11 +56,13 @@ export default function Home({latestEpisodes, allEpisodes} : HomeProps) {
           <h2>Todos os episódios</h2>
           <table cellSpacing={0}>
               <thead>
-                <th>Podcast</th>
-                <th>Integrantes</th>
-                <th>Data</th>
-                <th>Duração</th>
-                <th></th>
+                <tr>
+                  <th>Podcast</th>
+                  <th>Integrantes</th>
+                  <th>Data</th>
+                  <th>Duração</th>
+                  <th></th>
+                </tr>
               </thead>
               <tbody>
                 {allEpisodes.map(episode => {
@@ -68,7 +72,9 @@ export default function Home({latestEpisodes, allEpisodes} : HomeProps) {
                         <Image width={120} height={120} src={episode.thumbnail} alt={episode.title} objectFit="cover"/>
                       </td>
                       <td>
-                        <a href="">{episode.title}</a>
+                        <Link href={`/episodes/${episode.id}`}>
+                          <a>{episode.title}</a>
+                        </Link>
                       </td>
                       <td>{episode.members}</td>
                       <td style={{width: 100}}>{episode.publishedAt}</td>
@@ -106,7 +112,6 @@ export const getStaticProps: GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      description: episode.description,
       url: episode.file.url
     }
   })
