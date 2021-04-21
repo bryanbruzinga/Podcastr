59bfcf8bcb8941342d7ac5472cc1258d955b1003
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-04-21 08:35:15 -0300

    inclusão do axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
 
-export default function Home({episodes}) {
+type Episode = {
+  id: string,
+  title: string,
+  members: string,
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props : HomeProps) {
 
   return (
     <div>
       <h1>index</h1>
-      <p>{JSON.stringify(episodes)}</p>
+      <p>{JSON.stringify(props.episodes)}</p>
     </div>
   );
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('/episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
   return {
     props: {
       episodes: data,
