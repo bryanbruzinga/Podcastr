a4c6a703dde29024d9f99f91bf99cd6d64e4e674
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-04-22 09:12:10 -0300

    criado contexto do audio e aplicado

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -53,9 +53,27 @@ export default function Episode({episode}: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+
+    const { data } = await api.get('episodes', {
+        params: {
+            _limit: 2,
+            _sort: 'published_at',
+            _order: 'desc'
+        }
+    })
+
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    })
+    //This both variables above are only to generate static pages on build already loaded
     return {
-        paths: [],
+        paths,
         fallback: 'blocking'
+        //blocking only allows page to move forwards if loaded
     }
 }
 
